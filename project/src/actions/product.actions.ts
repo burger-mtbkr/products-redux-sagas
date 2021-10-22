47532f6f51033bedeade843331af375984eb6378
Lóan Burger <loan.burger@gmail.com>
2021-10-23 10:27:41 +1300

    Add actions for saving a product

--- project/src/actions/product.actions.ts
+++ project/src/actions/product.actions.ts
@@ -1,4 +1,4 @@
-import { ProductListItem } from 'src/models/';
+import { Product, ProductListItem } from 'src/models/';
 import { createAction } from '@reduxjs/toolkit';
 
 const PREFIX = 'PRODUCT';
@@ -6,18 +6,6 @@ const PREFIX = 'PRODUCT';
 export const initApp = createAction(`${PREFIX}_APP_INIT_APP`);
 
 export const isLoading = createAction<boolean>(`${PREFIX}_IS_LOADING`);
-
-export const fetchAllProductsAction = createAction(
-  `${PREFIX}/API/FETCH_ALL_PRODUCTS`,
-);
-
-export const fetchAllProductsDoneAction = createAction<ProductListItem[]>(
-  `${PREFIX}/API/FETCH_ALL_PRODUCTS_DONE`,
-);
-
-export const fetchAllProductsFailedAction = createAction<Error>(
-  `${PREFIX}/API/FETCH_ALL_PRODUCTS_FAIL`,
-);
 
 export const setDeleteModalOpenAction = createAction<boolean>(
   `${PREFIX}_SET_DELETE_MODAL_OPEN`,
@@ -29,4 +17,28 @@ export const setSelectedProductsAction = createAction<ProductListItem[]>(
 
 export const setHeaderTitleAction = createAction<string>(
   `APP_${PREFIX}_SET_HEADER_TITLE`,
+);
+
+export const fetchAllProductsAction = createAction(
+  `${PREFIX}/API/FETCH_ALL_PRODUCTS`,
+);
+
+export const fetchAllProductsDoneAction = createAction<ProductListItem[]>(
+  `${PREFIX}/API/FETCH_ALL_PRODUCTS_DONE`,
+);
+
+export const fetchAllProductsFailedAction = createAction<Error>(
+  `${PREFIX}/API/FETCH_ALL_PRODUCTS_FAILED`,
+);
+
+export const saveProductAction = createAction<Product>(
+  `${PREFIX}/API/SAVE_PRODUCT`,
+);
+
+export const saveProductActionDone = createAction(
+  `${PREFIX}/API/SAVE_PRODUCT_DONE`,
+);
+
+export const saveProductActionFailed = createAction<Error>(
+  `${PREFIX}/API/SAVE_PRODUCT_FAILED`,
 );
