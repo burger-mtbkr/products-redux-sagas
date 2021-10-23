9bddb1416eefd30242b921269b7579333293f6f9
Lóan Burger <loan.burger@gmail.com>
2021-10-23 15:23:14 +1300

    Added Save Product Saga

--- project/src/sagas/root.saga.ts
+++ project/src/sagas/root.saga.ts
@@ -1,6 +1,7 @@
 import { all } from 'redux-saga/effects';
 import { fetchAllProductsSaga } from 'src/sagas/fetchProducts.saga';
+import { saveProductSaga } from './saveProduct.saga';
 
 export default function* rootSaga() {
-  yield all([fetchAllProductsSaga()]);
+  yield all([fetchAllProductsSaga(), saveProductSaga()]);
 }
--- project/src/sagas/saveProduct.saga.ts
+++ project/src/sagas/saveProduct.saga.ts
@@ -1,24 +1,28 @@
 import { SagaIterator } from 'redux-saga';
 import { call, put, takeLatest } from 'redux-saga/effects';
 
-import { Product, ISaveProductResponse } from 'src/models/product.model';
+import { ISaveProductResponse } from 'src/models/product.model';
 import { saveProduct } from 'src/api';
 import {
-  fetchAllProductsAction,
+  saveProductAction,
   setSaveProductDoneAction,
   isSavingAction,
 } from 'src/actions';
 
-export function* saveProductSaga(product: Product): SagaIterator {
-  yield takeLatest(fetchAllProductsAction, function* (): SagaIterator {
+export function* saveProductSaga(): SagaIterator {
+  yield takeLatest(saveProductAction, function* (action): SagaIterator {
     try {
+      debugger;
       yield put(isSavingAction(true));
-      const response: ISaveProductResponse = yield call(saveProduct, product);
+      const response: ISaveProductResponse = yield call(
+        saveProduct,
+        action.payload,
+      );
       yield put(setSaveProductDoneAction(response));
     } catch (error) {
       yield put(
         setSaveProductDoneAction({
-          product,
+          product: action.payload,
           error: error as Error,
           isSuccessful: false,
         }),
